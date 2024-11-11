47c17a81c41c34ff66818b81cd882be046371929
Mike Purvis <mikedotexe@gmail.com>
2024-11-10 20:31:52 -0800

    slapping away assumptions of browser environment, check for window

--- packages/core/src/lib/services/storage/web-storage.service.ts
+++ packages/core/src/lib/services/storage/web-storage.service.ts
@@ -3,24 +3,34 @@ import type { StorageService } from "./storage.service.types.js";
 export class WebStorageService implements StorageService {
   getItem(key: string): Promise<string | null> {
     return new Promise((resolve) => {
+      if (typeof window === "undefined") {
+        // We're on the server; return null.
+        resolve(null);
+        return;
+      }
       const value = localStorage.getItem(key);
-
       resolve(value);
     });
   }
 
   setItem(key: string, value: string): Promise<void> {
     return new Promise((resolve) => {
+      if (typeof window === "undefined") {
+        resolve();
+        return;
+      }
       localStorage.setItem(key, value);
-
       resolve();
     });
   }
 
   removeItem(key: string): Promise<void> {
     return new Promise((resolve) => {
+      if (typeof window === "undefined") {
+        resolve();
+        return;
+      }
       localStorage.removeItem(key);
-
       resolve();
     });
   }
